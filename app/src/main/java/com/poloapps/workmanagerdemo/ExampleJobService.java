81a9362184cad05cc33c6c572e8eeb5a0ed31ccf
Marko <ms7538@gmail.com>
2019-03-30 21:13:04 -0600

    canceljob from within

--- app/src/main/java/com/poloapps/workmanagerdemo/ExampleJobService.java
+++ app/src/main/java/com/poloapps/workmanagerdemo/ExampleJobService.java
@@ -1,12 +1,14 @@
 package com.poloapps.workmanagerdemo;
 
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
 import android.app.job.JobService;
 import android.util.Log;
 
 public class ExampleJobService extends JobService {
     private static final String TAG = "ExampleJobService";
     private boolean jobCancelled = false;
+    private static final int JS_ID = 943292346;
 
     @Override
     public boolean onStartJob(JobParameters params) {
@@ -15,20 +17,17 @@ public class ExampleJobService extends JobService {
         return true;
     }
 
-    private void doBackgroundWork(final JobParameters params){
+    private void doBackgroundWork(final JobParameters params ){
         new Thread(new Runnable() {
             @Override
             public void run() {
+
                 for (int i = 0; i < 10; i++){
                     Log.d(TAG, "run: " + i);
-                    if (jobCancelled){
-                        return;
-                    }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    if(i == 5){cancelJob();}
+                    if (jobCancelled){ return; }
+                    try {Thread.sleep(500);}
+                    catch (InterruptedException e) {e.printStackTrace(); }
                 }
                 Log.d(TAG, "Job Finished");
                 jobFinished(params, false);
@@ -41,5 +40,11 @@ public class ExampleJobService extends JobService {
         Log.d(TAG, "Job cancelled before completion");
         jobCancelled = true;
         return true;
+    }
+    public void cancelJob(){
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        assert scheduler != null;
+        scheduler.cancel(JS_ID);
+        Log.d(TAG, "Job stopped from within");
     }
 }
